f23d464e6a1d6020b7d88fb5da06e0d8596ac6c8
司昌旭 <scx567888@outlook.com>
2021-01-27 14:44:16 +0800

    修复无法从表单中读取数据的 bug

--- src/main/java/cool/scx/server/ScxRouteHandler.java
+++ src/main/java/cool/scx/server/ScxRouteHandler.java
@@ -1,15 +1,15 @@
 package cool.scx.server;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import cool.scx.annotation.ScxMapping;
 import cool.scx.util.ObjectUtils;
-import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Map;
+import java.lang.reflect.Parameter;
 
 public final class ScxRouteHandler {
     public final Method method;
@@ -22,62 +22,106 @@ public final class ScxRouteHandler {
         this.scxMapping = scxMapping;
     }
 
-    private static Object getValueFormPathAndQuery(String parameterName, Class<?> parameterType, Map<String, String> pathParams, MultiMap queryParams) {
-        //body 转换失败 尝试采用 pathParams
-        var tempParams = pathParams.get(parameterName);
-        //从 路径参数获取失败
-        if (tempParams == null) {
-            //尝试从 查询参数
-            tempParams = queryParams.get(parameterName);
-        }
-        return ObjectUtils.parseSimpleType(tempParams, parameterType);
-    }
-
     public Object getResult(RoutingContext ctx) {
         var parameters = method.getParameters();
         var parameterNames = ObjectUtils.u.getParameterNames(method);
-        var handlerParams = new Object[parameters.length];
-        String jsonStr = "";
-        //一共有三种参数来源 优先级 body > path > query
-        if (ctx.request().method() != HttpMethod.GET) {
-            jsonStr = ctx.getBodyAsString();
-        }
-        JsonNode rootJsonNode = null;
-        try {
-            rootJsonNode = ObjectUtils.OBJECT_MAPPER.readTree(jsonStr);
-        } catch (Exception ignored) {
+        var handlerParamsFromJson = getHandlerParamsFromJson(ctx, parameters, parameterNames);
+        var handlerParamsFromFormAttributes = getHandlerParamsFromFormAttributes(ctx, parameters, parameterNames);
+        var handlerParamsFromPath = getHandlerParamsFromPath(ctx, parameters, parameterNames);
+        var handlerParamsFromQuery = getHandlerParamsFromQuery(ctx, parameters, parameterNames);
 
-        }
-        var pathParams = ctx.pathParams();
-        var queryParams = ctx.queryParams();
-        for (int i = 0; i < parameters.length; i++) {
+        var finalHandlerParams = new Object[parameters.length];
+        for (int i = 0; i < finalHandlerParams.length; i++) {
             var nowType = parameters[i].getType();
-            var nowName = parameterNames[i];
             if (nowType == RoutingContext.class) {
-                handlerParams[i] = ctx;
+                finalHandlerParams[i] = ctx;
                 continue;
             }
-            try {
-                //先尝试将 body 中的数据进行转换
-                if (parameters.length == 1) {
-                    handlerParams[i] = ObjectUtils.OBJECT_MAPPER.readValue(jsonStr, nowType);
-                } else {
-                    handlerParams[i] = ObjectUtils.OBJECT_MAPPER.treeToValue(rootJsonNode.get(nowName), nowType);
-                    if ("".equals(handlerParams[i]) || handlerParams[i] == null) {
-                        handlerParams[i] = getValueFormPathAndQuery(nowName, nowType, pathParams, queryParams);
-                    }
-                }
-            } catch (Exception e) {
-                handlerParams[i] = getValueFormPathAndQuery(nowName, nowType, pathParams, queryParams);
+            if (handlerParamsFromJson[i] != null) {
+                finalHandlerParams[i] = handlerParamsFromJson[i];
+                continue;
+            }
+            if (handlerParamsFromFormAttributes[i] != null) {
+                finalHandlerParams[i] = handlerParamsFromFormAttributes[i];
+                continue;
+            }
+            if (handlerParamsFromPath[i] != null) {
+                finalHandlerParams[i] = handlerParamsFromPath[i];
+                continue;
+            }
+            if (handlerParamsFromQuery[i] != null) {
+                finalHandlerParams[i] = handlerParamsFromQuery[i];
             }
         }
 
         try {
-            return method.invoke(example, handlerParams);
+            return method.invoke(example, finalHandlerParams);
         } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
             e.getCause();
             return "服务器发生错误";
         }
 
     }
+
+    public static Object[] getHandlerParamsFromJson(RoutingContext ctx, Parameter[] parameters, String[] parameterNames) {
+        var handlerParams = new Object[parameters.length];
+        String jsonStr = ctx.request().method() != HttpMethod.GET ? ctx.getBodyAsString() : "";
+        JsonNode rootJsonNode = null;
+        try {
+            rootJsonNode = ObjectUtils.OBJECT_MAPPER.readTree(jsonStr);
+        } catch (JsonProcessingException ignored) {
+
+        }
+        for (int i = 0; i < handlerParams.length; i++) {
+            var nowType = parameters[i].getType();
+            var nowName = parameterNames[i];
+            //先尝试将 body 中的数据进行转换
+            if (parameters.length == 1) {
+                try {
+                    handlerParams[i] = ObjectUtils.OBJECT_MAPPER.readValue(jsonStr, nowType);
+                } catch (Exception e) {
+                    try {
+                        handlerParams[i] = ObjectUtils.OBJECT_MAPPER.treeToValue(rootJsonNode.get(nowName), nowType);
+                    } catch (Exception ignored) {
+
+                    }
+                }
+            } else {
+                try {
+                    handlerParams[i] = ObjectUtils.OBJECT_MAPPER.treeToValue(rootJsonNode.get(nowName), nowType);
+                } catch (Exception ignored) {
+
+                }
+            }
+        }
+        return handlerParams;
+    }
+
+    public static Object[] getHandlerParamsFromFormAttributes(RoutingContext ctx, Parameter[] parameters, String[] parameterNames) {
+        var handlerParams = new Object[parameters.length];
+        var request = ctx.request();
+        for (int i = 0; i < handlerParams.length; i++) {
+            handlerParams[i] = ObjectUtils.parseSimpleType(request.getFormAttribute(parameterNames[i]), parameters[i].getType());
+        }
+        return handlerParams;
+    }
+
+    public static Object[] getHandlerParamsFromPath(RoutingContext ctx, Parameter[] parameters, String[] parameterNames) {
+        var pathParams = ctx.pathParams();
+        var handlerParams = new Object[parameters.length];
+        for (int i = 0; i < handlerParams.length; i++) {
+            handlerParams[i] = ObjectUtils.parseSimpleType(pathParams.get(parameterNames[i]), parameters[i].getType());
+        }
+        return handlerParams;
+    }
+
+    public static Object[] getHandlerParamsFromQuery(RoutingContext ctx, Parameter[] parameters, String[] parameterNames) {
+        var queryParams = ctx.queryParams();
+        var handlerParams = new Object[parameters.length];
+        for (int i = 0; i < handlerParams.length; i++) {
+            handlerParams[i] = ObjectUtils.parseSimpleType(queryParams.get(parameterNames[i]), parameters[i].getType());
+        }
+        return handlerParams;
+    }
+
 }
