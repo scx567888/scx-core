d0a3ffd5ae54402f435ebb48407b92e184338c91
司昌旭 <scx567888@outlook.com>
2021-02-04 23:23:25 +0800

    1.修改 maven 构建参数

--- src/main/java/cool/scx/boot/ScxListener.java
+++ src/main/java/cool/scx/boot/ScxListener.java
@@ -7,28 +7,28 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public final class ScxListener {
     static {
-        try {
-            var fis = new FileInputStream(PackageUtils.getFileByAppRoot("\\session.cache"));
-            ObjectInputStream objectInputStream = new ObjectInputStream(fis);
-            var o = (ConcurrentHashMap<String, String>) objectInputStream.readObject();
-            for (var entry : o.entrySet()) {
-                ScxContext.addUserToSession(entry.getKey(), entry.getValue());
-            }
-            objectInputStream.close();
-        } catch (Exception ignored) {
-
-        }
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                var fos = new FileOutputStream(PackageUtils.getFileByAppRoot("\\session.cache"));
-                var objectOutputStream = new ObjectOutputStream(fos);
-//                objectOutputStream.writeObject(ScxContext.session);
-                objectOutputStream.close();
-
-            } catch (IOException ignored) {
-
-            }
-        }));
+//        try {
+//            var fis = new FileInputStream(PackageUtils.getFileByAppRoot("\\session.cache"));
+//            ObjectInputStream objectInputStream = new ObjectInputStream(fis);
+//            var o = (ConcurrentHashMap<String, String>) objectInputStream.readObject();
+//            for (var entry : o.entrySet()) {
+//                ScxContext.addUserToSession(entry.getKey(), entry.getValue());
+//            }
+//            objectInputStream.close();
+//        } catch (Exception ignored) {
+//
+//        }
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//            try {
+//                var fos = new FileOutputStream(PackageUtils.getFileByAppRoot("\\session.cache"));
+//                var objectOutputStream = new ObjectOutputStream(fos);
+////                objectOutputStream.writeObject(ScxContext.session);
+//                objectOutputStream.close();
+//
+//            } catch (IOException ignored) {
+//
+//            }
+//        }));
     }
 
     public static void init() {
