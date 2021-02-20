b82b1555c7b6721c4372cab8f018fbaf65675cd9
司昌旭 <scx567888@outlook.com>
2021-02-20 13:14:22 +0800

    0.5.1

--- src/main/java/cool/scx/boot/ScxConfig.java
+++ src/main/java/cool/scx/boot/ScxConfig.java
@@ -35,7 +35,7 @@ public final class ScxConfig {
     /**
      * 核心包版本
      */
-    public static final String coreVersion = "0.5.0";
+    public static final String coreVersion = "0.5.1";
     /**
      * Constant <code>scxConfigJsonNode</code>
      */
