137ecd15d021af6f9a2e7f2341911401c454b774
司昌旭 <scx567888@outlook.com>
2021-02-25 13:05:08 +0800

    0.8.2

--- src/main/java/cool/scx/server/handler/LoginAndPermsHandler.java
+++ src/main/java/cool/scx/server/handler/LoginAndPermsHandler.java
@@ -14,12 +14,17 @@ public interface LoginAndPermsHandler {
     /**
      * <p>noLogin.</p>
      *
-     * @param routingContext a {@link io.vertx.ext.web.RoutingContext} object.
+     * @param context a {@link io.vertx.ext.web.RoutingContext} object.
      */
     default void noLoginByCookie(RoutingContext context) {
         Json.fail(Json.ILLEGAL_TOKEN, "未登录").sendToClient(context);
     }
 
+    /**
+     * <p>noLoginByHeader.</p>
+     *
+     * @param context a {@link io.vertx.ext.web.RoutingContext} object.
+     */
     default void noLoginByHeader(RoutingContext context) {
         Json.fail(Json.ILLEGAL_TOKEN, "未登录").sendToClient(context);
     }
@@ -27,12 +32,17 @@ public interface LoginAndPermsHandler {
     /**
      * <p>noPerms.</p>
      *
-     * @param routingContext a {@link io.vertx.ext.web.RoutingContext} object.
+     * @param context a {@link io.vertx.ext.web.RoutingContext} object.
      */
     default void noPermsByCookie(RoutingContext context) {
         Json.fail(Json.NO_PERMISSION, "没有权限").sendToClient(context);
     }
 
+    /**
+     * <p>noPermsByHeader.</p>
+     *
+     * @param context a {@link io.vertx.ext.web.RoutingContext} object.
+     */
     default void noPermsByHeader(RoutingContext context) {
         Json.fail(Json.NO_PERMISSION, "没有权限").sendToClient(context);
     }
