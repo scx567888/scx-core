1480d4898c8e93e4c1e476f5c9d72d2f69ea25f2
司昌旭 <scx567888@outlook.com>
2021-02-25 12:13:28 +0800

    0.7.12

--- src/main/java/cool/scx/enumeration/ScanPackageVisitResult.java
+++ src/main/java/cool/scx/enumeration/ScanPackageVisitResult.java
@@ -4,7 +4,7 @@ package cool.scx.enumeration;
  * <p>ScanPackageVisitResult class.</p>
  *
  * @author scx56
- * @version $Id: $Id
+ * @version 0.7.12
  */
 public enum ScanPackageVisitResult {
     CONTINUE,
