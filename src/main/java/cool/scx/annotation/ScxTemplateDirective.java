b729eab19c0da42057c7b2e400d82ea4c5c3f5c8
司昌旭 <scx567888@outlook.com>
2021-03-05 18:37:09 +0800

    1,添加 模板自定义指令注解

---
+++ src/main/java/cool/scx/annotation/ScxTemplateDirective.java
@@ -0,0 +1,19 @@
+package cool.scx.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义 模板指令
+ * 使用此注解时 必须同时继承
+ * BaseTemplateDirective
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface ScxTemplateDirective {
+}
