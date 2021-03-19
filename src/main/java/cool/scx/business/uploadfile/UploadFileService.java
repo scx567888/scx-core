a5ec26af77f75bacb9a991c26430df49b7fd562a
司昌旭 <scx567888@outlook.com>
2021-03-19 19:02:48 +0800

    1.0.5

--- src/main/java/cool/scx/business/uploadfile/UploadFileService.java
+++ src/main/java/cool/scx/business/uploadfile/UploadFileService.java
@@ -3,6 +3,7 @@ package cool.scx.business.uploadfile;
 import cool.scx.annotation.ScxService;
 import cool.scx.base.BaseService;
 import cool.scx.bo.Param;
+import cool.scx.enumeration.SortType;
 
 /**
  * <p>UploadFileService class.</p>
@@ -19,7 +20,7 @@ public class UploadFileService extends BaseService<UploadFile> {
      * @return a {@link cool.scx.business.uploadfile.UploadFile} object.
      */
     public UploadFile findFileByMd5(String fileMD5) {
-        var p = new Param<>(new UploadFile());
+        var p = new Param<>(new UploadFile()).addOrderBy("uploadTime", SortType.DESC);
         p.queryObject.fileMD5 = fileMD5;
         return get(p);
     }
