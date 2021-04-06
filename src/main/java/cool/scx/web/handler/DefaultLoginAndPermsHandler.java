5be0f9d1a868c300ac9a063391ef8941f71a949e
司昌旭 <scx567888@outlook.com>
2021-04-06 22:31:55 +0800

    1.0.10

--- src/main/java/cool/scx/web/handler/DefaultLoginAndPermsHandler.java
+++ src/main/java/cool/scx/web/handler/DefaultLoginAndPermsHandler.java
@@ -1,5 +1,10 @@
 package cool.scx.web.handler;
 
+import cool.scx.enumeration.Device;
+import cool.scx.util.Ansi;
+import cool.scx.vo.Json;
+import io.vertx.ext.web.RoutingContext;
+
 /**
  * <p>DefaultLoginAndPermsHandler class.</p>
  *
@@ -8,4 +13,29 @@ package cool.scx.web.handler;
  */
 public class DefaultLoginAndPermsHandler implements LoginAndPermsHandler {
 
+    @Override
+    public void noLogin(Device device, RoutingContext context) {
+        if (device == Device.ADMIN) {
+            Json.fail(Json.ILLEGAL_TOKEN, "未登录").sendToClient(context);
+        } else if (device == Device.ANDROID) {
+            Json.fail(Json.ILLEGAL_TOKEN, "未登录").sendToClient(context);
+        } else if (device == Device.APPLE) {
+            Json.fail(Json.ILLEGAL_TOKEN, "未登录").sendToClient(context);
+        } else if (device == Device.WEBSITE) {
+            Ansi.OUT.red("未登录").ln();
+        }
+    }
+
+    @Override
+    public void noPerms(Device device, RoutingContext context) {
+        if (device == Device.ADMIN) {
+            Json.fail(Json.NO_PERMISSION, "没有权限").sendToClient(context);
+        } else if (device == Device.ANDROID) {
+            Json.fail(Json.NO_PERMISSION, "没有权限").sendToClient(context);
+        } else if (device == Device.APPLE) {
+            Json.fail(Json.NO_PERMISSION, "没有权限").sendToClient(context);
+        } else if (device == Device.WEBSITE) {
+            Ansi.OUT.red("没有权限").ln();
+        }
+    }
 }
--- src/main/java/cool/scx/web/handler/LoginAndPermsHandler.java
+++ src/main/java/cool/scx/web/handler/LoginAndPermsHandler.java
@@ -1,52 +1,31 @@
 package cool.scx.web.handler;
 
 import cool.scx.enumeration.Device;
-import cool.scx.util.Ansi;
-import cool.scx.vo.Json;
 import io.vertx.ext.web.RoutingContext;
 
 /**
  * <p>LoginAndPermsHandler interface.</p>
  *
- * @author scx56
+ * @author 司昌旭
  * @version $Id: $Id
  */
 public interface LoginAndPermsHandler {
 
     /**
-     * <p>noLogin.</p>
+     * 未登录 handler
      *
      * @param context a {@link io.vertx.ext.web.RoutingContext} object.
      * @param device  a {@link cool.scx.enumeration.Device} object.
      */
-    default void noLogin(Device device, RoutingContext context) {
-        if (device == Device.ADMIN) {
-            Json.fail(Json.ILLEGAL_TOKEN, "未登录").sendToClient(context);
-        } else if (device == Device.ANDROID) {
-            Json.fail(Json.ILLEGAL_TOKEN, "未登录").sendToClient(context);
-        } else if (device == Device.APPLE) {
-            Json.fail(Json.ILLEGAL_TOKEN, "未登录").sendToClient(context);
-        } else if (device == Device.WEBSITE) {
-            Ansi.OUT.red("未登录").ln();
-        }
-    }
+    void noLogin(Device device, RoutingContext context);
+
 
     /**
-     * <p>noPerms.</p>
+     * 无权限 handler
      *
      * @param context a {@link io.vertx.ext.web.RoutingContext} object.
      * @param device  a {@link cool.scx.enumeration.Device} object.
      */
-    default void noPerms(Device device, RoutingContext context) {
-        if (device == Device.ADMIN) {
-            Json.fail(Json.NO_PERMISSION, "没有权限").sendToClient(context);
-        } else if (device == Device.ANDROID) {
-            Json.fail(Json.NO_PERMISSION, "没有权限").sendToClient(context);
-        } else if (device == Device.APPLE) {
-            Json.fail(Json.NO_PERMISSION, "没有权限").sendToClient(context);
-        } else if (device == Device.WEBSITE) {
-            Ansi.OUT.red("没有权限").ln();
-        }
-    }
+    void noPerms(Device device, RoutingContext context);
 
 }
