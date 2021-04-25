ed02a7606e19b3833848acfdf162c8f482f39492
司昌旭 <scx567888@outlook.com>
2021-04-25 23:01:07 +0800

    1.0.16

--- src/main/java/cool/scx/config/ScxConfig.java
+++ src/main/java/cool/scx/config/ScxConfig.java
@@ -393,7 +393,7 @@ public final class ScxConfig {
      * @return a {@link java.lang.String} object.
      */
     public static String scxVersion() {
-        return "1.0.15";
+        return "1.0.16";
     }
 
     /**
