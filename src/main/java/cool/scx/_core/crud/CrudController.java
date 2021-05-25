e88f2d4af459f388c69aaf7e12e4a4995d740b30
司昌旭 <scx567888@outlook.com>
2021-05-25 17:22:02 +0800

    1.1.2

--- src/main/java/cool/scx/_core/crud/CrudController.java
+++ src/main/java/cool/scx/_core/crud/CrudController.java
@@ -34,7 +34,6 @@ public class CrudController {
      * @throws HttpResponseException service 未找到
      */
     @SuppressWarnings("unchecked")
-    @SuppressWarnings("unchecked")
     private static <T extends BaseModel> BaseService<T> getBaseService(String modelName) throws HttpResponseException {
         try {
             var o = ScxContext.getBean(ScxContext.getClassByName(modelName.toLowerCase() + "service"));
