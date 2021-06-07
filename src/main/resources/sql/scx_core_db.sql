792909edbf8ef5464ae96f66ec66b1b56e4f8cb5
司昌旭 <scx567888@outlook.com>
2021-06-07 14:52:27 +0800

    1,初始化 sql 脚本bug修复

--- src/main/resources/sql/scx_core_db.sql
+++ src/main/resources/sql/scx_core_db.sql
@@ -2,8 +2,8 @@
 -- 核心功能表
 
 -- 添加初始管理员用户 用户名 admin 密码 123456
-INSERT INTO scx.core_user (username, salt, password, is_admin)
+INSERT INTO scx.auth_user (username, salt, password, is_admin)
 VALUES ('admin', 'b1bee9691622e55f', 'qEB8gv1T/ZPaKwU6dN9ZWi4UNdhGXJOXjlZSOryemqWUlHoC5FKRwJOxludkytB2', true);
 -- 添加默认 license
-INSERT INTO scx.core_license (id, create_date, update_date, is_deleted, flag, last_time)
+INSERT INTO scx.auth_license (id, create_date, update_date, is_deleted, flag, last_time)
 VALUES (1, '2020-11-02 11:31:24', '2020-11-02 11:36:05', 0, 1, '2020-11-02');
