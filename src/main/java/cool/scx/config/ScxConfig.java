7364f3074aaf5ded6ec3d06ba22644503debd41e
司昌旭 <scx567888@outlook.com>
2021-06-09 00:15:52 +0800

    1.1.14

--- src/main/java/cool/scx/config/ScxConfig.java
+++ src/main/java/cool/scx/config/ScxConfig.java
@@ -27,7 +27,7 @@ public final class ScxConfig {
     /**
      * SCX 版本号
      */
-    public static final String SCX_VERSION = "1.1.13";
+    public static final String SCX_VERSION = "1.1.14";
 
     /**
      * APP KEY 常量
