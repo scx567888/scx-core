13bc115ef68bf967d1b4a7d409067308506258b2
司昌旭 <scx567888@outlook.com>
2021-06-09 09:00:30 +0800

    1.1.15

--- src/main/java/cool/scx/exception/BadRequestException.java
+++ src/main/java/cool/scx/exception/BadRequestException.java
@@ -3,18 +3,35 @@ package cool.scx.exception;
 import cool.scx.vo.Json;
 import io.vertx.ext.web.RoutingContext;
 
+/**
+ * <p>BadRequestException class.</p>
+ *
+ * @author 司昌旭
+ * @version 1.1.15
+ */
 public class BadRequestException extends HttpRequestException {
 
     private Throwable throwable;
 
+    /**
+     * <p>Constructor for BadRequestException.</p>
+     */
     public BadRequestException() {
 
     }
 
+    /**
+     * <p>Constructor for BadRequestException.</p>
+     *
+     * @param throwable a {@link java.lang.Throwable} object
+     */
     public BadRequestException(Throwable throwable) {
         this.throwable = throwable;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void exceptionHandler(RoutingContext ctx) {
         if (throwable == null) {
