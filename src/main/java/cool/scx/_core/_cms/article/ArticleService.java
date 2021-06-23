190618bbfd733c2344fa9ead2f9246e2babe25d8
司昌旭 <scx567888@outlook.com>
2021-06-23 11:12:16 +0800

    1,CMS 模块兼容性调整

--- src/main/java/cool/scx/_core/_cms/article/ArticleService.java
+++ src/main/java/cool/scx/_core/_cms/article/ArticleService.java
@@ -24,9 +24,7 @@ public class ArticleService extends BaseService<Article> {
      */
     public Object testListAll(Map<String, Object> params) {
         var author = params.get("author") != null ? params.get("author").toString() : "";
-        var p = new QueryParam();
-        Long count = count(p);
-        if (count < 20) {
+        if (count() < 20) {
             var l = new ArrayList<Article>();
             for (int i = 0; i < 50; i++) {
                 var a = new Article();
