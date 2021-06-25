b0b45ebf227bccf645c35a5a91a3df39b18e9262
司昌旭 <scx567888@outlook.com>
2021-06-25 22:54:26 +0800

    1,groupby 优化

--- src/main/java/cool/scx/bo/GroupBy.java
+++ src/main/java/cool/scx/bo/GroupBy.java
@@ -22,13 +22,13 @@ public class GroupBy {
     /**
      * 一个实体类的 class 对象 当不为空时可以对传进来的参数进行数据校验
      */
-    private Class<? extends BaseModel> entityClass = null;
+    private final Class<?> entityClass;
 
     /**
      * <p>Constructor for GroupBy.</p>
      */
     public GroupBy() {
-
+        this.entityClass = null;
     }
 
     /**
@@ -37,6 +37,7 @@ public class GroupBy {
      * @param fieldName a {@link java.lang.String} object
      */
     public GroupBy(String fieldName) {
+        this.entityClass = null;
         add(fieldName);
     }
 
@@ -51,7 +52,7 @@ public class GroupBy {
     /**
      * 创建一个 OrderBy 对象 (添加排序字段时会根据 entityClass 校验数据)
      */
-    public GroupBy(Class<? extends BaseModel> entityClass) {
+    public GroupBy(Class<?> entityClass) {
         this.entityClass = entityClass;
     }
 
@@ -70,22 +71,11 @@ public class GroupBy {
     }
 
     /**
-     * 添加一个 分组字段
-     *
-     * @param fieldName 分组字段的名称 (注意是实体类的字段名 , 不是数据库中的字段名)
-     * @return 本身, 方便链式调用
-     */
-    public GroupBy forceAdd(final String fieldName) {
-        groupByList.add(fieldName);
-        return this;
-    }
-
-    /**
      * 检查 OrderByBody 此处做两个校验<br>
      * 第一个是 当 entityClass 存在时会先校验 orderByColumn 是否存在于 实体类中的字段<br>
      * 当 entityClass 不存在时则不会校验
      * <br>
-     * 第二个是对 数据重复进行校验 理论上 orderBy 不应该存在相同的字段
+     * 第二个是对 数据重复进行校验 理论上 GroupBy 不应该存在相同的字段
      *
      * @return 检查的结果 只有为 true 时才会向列表中添加
      */
@@ -104,7 +94,7 @@ public class GroupBy {
         if (!contains) {
             return true;
         } else {
-            Ansi.OUT.brightRed("已经添加过相同的 GroupBy 字段 , 内容是: " + groupByColumn + " , 若要强行覆盖请使用 forceAdd !!!").ln();
+            Ansi.OUT.brightRed("已跳过添加过相同的 GroupBy 字段 , 内容是: " + groupByColumn + " !!!").ln();
             return false;
         }
 
